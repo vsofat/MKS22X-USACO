240de1790002ccae12446c8fa5649e581bfb97fc
vsofat <vsofat00@stuy.edu>
2019-03-06 11:46:45 -0500

    classwork march 6

--- USACO.java
+++ USACO.java
@@ -4,6 +4,22 @@ import java.util.*;
 public class USACO{
 
   public static int bronze (String filename) throws FileNotFoundException{
+    File file = new File (filename);
+    Scanner scanner = new Scanner(filename);
+    int R = 0;
+    int C = 0;
+    int E = 0;
+    int N = 0;
+    while (scanner.hasNextLine()){
+    int len = scanner.nextLine().length();
+    String info = scanner.nextLine();
+    String[] first = ;
+    for (int row = 0; row < len; row++){
+      if (row == 0){
+         first = info.split("");
+        }
+      }
+    }
 
   }
 }
