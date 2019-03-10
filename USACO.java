42d6dd9410c4ad6e4da6e05259ce10275e771cba
vsofat <vsofat00@stuy.edu>
2019-03-10 17:03:00 -0400

    finds max value for both given orders(N)

--- USACO.java
+++ USACO.java
@@ -36,10 +36,30 @@ public class USACO{
         board[rows][cols] = Integer.parseInt(input.next()) ;
       }
     }
-    //System.out.println(toString(board));
-    return 0;
+
+    for (int modify = 0; modify < N; modify++){
+      int startRow = Integer.parseInt(input.next()) - 1;
+      int startCol = Integer.parseInt(input.next()) - 1;
+      int stompVal = Integer.parseInt(input.next());
+      int maxVal = 0;
+      for (int row = 0; row < startRow + 3; row++){
+        for (int col = 0; col < startCol + 3; col++){
+            if (board[row][col] > maxVal){
+              maxVal = board[row][col];
+            } // we now have a max value
+      }
+    }
+    System.out.println("value of max: " + maxVal);
   }
 
+    System.out.println(toString(board));
+    System.out.println("value of R: " + R);
+    System.out.println("value of C: " + C);
+    System.out.println("value of E: " + E);
+    System.out.println("value of N: " + N);
+
+    return 0;
+  }
 
   public static String toString(int[][]array){
     String result = "";
